5d24b16a509238e9df5272828b6aa530b5e392f7
ashikpkmadhupam <ashikpkmadhupam@gmail.com>
2024-03-12 15:12:04 +0530

    Cleanup

--- src/main/java/com/survey/invoicegenerator/controller/Controller.java
+++ src/main/java/com/survey/invoicegenerator/controller/Controller.java
@@ -1,7 +1,5 @@
 package com.survey.invoicegenerator.controller;
 
-
-import com.itextpdf.text.DocumentException;
 import com.survey.invoicegenerator.model.InvoiceData;
 import com.survey.invoicegenerator.service.PDFService;
 import lombok.RequiredArgsConstructor;
@@ -9,19 +7,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/invoice/pdf")
 @RequiredArgsConstructor
 public class Controller {
 
     private final PDFService pdfService;
-
-    @GetMapping("/generate")
-    public ResponseEntity<Resource> generateItext() throws DocumentException, IOException {
-        return pdfService.generateInvoiceIText();
-    }
 
     @PostMapping("/generate")
     public ResponseEntity<Resource> generatePDF(@RequestBody InvoiceData invoiceData) {
